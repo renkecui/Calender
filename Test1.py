ae91f03d9e3c0455785a59015747d3b250659735
Renkecui@gmail.com <115436880+renkecui@users.noreply.github.com>
2022-12-03 17:56:26 -0500

    Testing Files

---
+++ Test1.py
@@ -0,0 +1,7 @@
+import calendar
+
+yy = 2022
+mm = 12
+
+calenData = calendar.month(yy,mm)
+print(calenData) 
---
+++ Test2.py
@@ -0,0 +1,64 @@
+import calendar
+
+# Print a calendar
+#print(calendar.calendar(2022, w=4, l=2))
+
+# Is it a leap year
+# print(calendar.isleap(2021))
+
+# How many leap days?
+# print(calendar.leapdays(1984, 2021))
+
+# Days of the months
+# print(calendar.month(2021, 10))
+
+# List of the days and the weeks
+# print(calendar.monthcalendar(2021, 10))
+
+# The first weekday
+# print(calendar.firstweekday())
+
+# the starting day number of week(0 as monday), second, the number of days in the month
+# print(calendar.monthrange(2021, 8))
+
+# Which day of the week was it?
+# print(calendar.weekday(1988, 10, 9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print (calendar.calendar(2021, w=2,l=1))
+#print(calendar.isleap(2021))
+#print(calendar.leapdays(1984,2021))
+#print(calendar.month(2021, 10))
+#print(calendar.monthcalendar(2021,10))
+#print(calendar.firstweekday())
+#print(calendar.monthrange(2021,8))
+#print(calendar.weekday(1984,1,10))
