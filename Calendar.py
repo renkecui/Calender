721f46ce9b0197cdb94bde51f6a9706c703958c5
Renkecui@gmail.com <115436880+renkecui@users.noreply.github.com>
2022-12-04 14:23:41 -0500

    update 1

---
+++ Calendar.py
@@ -0,0 +1,20 @@
+import calendar
+
+class features:
+    WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    def __init__(self):
+        pass
+    def year_view(self):
+        pass
+    def month_view(self):
+        pass
+    def week_view(self):
+        pass
+    def day_view(self):
+        pass
+    def add_event(self, event_name, note, add_due_date = False, due_date = None):
+        pass
+    def show_to_do_list(self):
+        pass
+    
+        
