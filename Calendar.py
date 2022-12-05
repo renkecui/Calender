34e28779b1a7190f04fb687bba89d90ce9c35419
Renkecui@gmail.com <115436880+renkecui@users.noreply.github.com>
2022-12-04 20:18:46 -0500

    email

--- Calendar.py
+++ Calendar.py
@@ -1,4 +1,9 @@
 import calendar
+import tkinter as tk
+from tkinter import ttk
+from model import Model
+from view import View
+from controller import Controller
 
 class features:
     WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
@@ -18,3 +23,23 @@ class features:
         pass
     
         
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Tkinter MVC Demo')
+
+        # create a model
+        model = Model('hello@pythontutorial.net')
+
+        # create a view and place it on the root window
+        view = View(self)
+        view.grid(row=0, column=0, padx=10, pady=10)
+
+        # create a controller
+        controller = Controller(model, view)
+
+        # set the controller to view
+        view.set_controller(controller)
+
+         
---
+++ main.py
@@ -0,0 +1,8 @@
+import re
+import tkinter as tk
+from tkinter import ttk
+from Calendar import App
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()   
